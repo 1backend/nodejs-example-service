4774aa9011ca388f2dfb532c3560f6672ef256f1
Janos Dobronszki <dobronszki@gmail.com>
2017-12-10 14:27:17 +0100

    Added rectangle endpoint example

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-exports.sayHelloInEnglish = (req, res) => {
-  res.send('Hi! This endpoint is using an imported service');
+exports.hi = (req, res) => {
+  res.send(JSON.stringify('Hi! This endpoint is using an imported service'));
+};
+
+exports.calculateRectangleArea = (req, res) => {
+  var rect = req.body.rect;
+  let unit = req.body.Unit;
+  if (!unit) {
+    unit = 'cm2';
+  }
+  const area = rect.sideA * rect.sideA;
+
+  rsp.send(JSON.stringify(area + unit));
 };
